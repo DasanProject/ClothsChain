3419ba7781eef443779a855b59e500f0e0e538ec
“glxyjiayou <703567003@qq.com>
2018-10-15 20:57:26 +0800

    lxy

---
+++ ClothChain/src/main/java/com/example/demo/controller/Tab_customerController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.biz.Tab_customerBiz;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/customer")
+
+public class Tab_customerController {
+    @Resource
+    private Tab_customerBiz biz;
+    @RequestMapping("/queryCustomer")
+    @ResponseBody
+    public List<Map>  lxy_queryCustomer(){//查询客户
+         System.out.println("aa");
+         List<Map> list=biz.queryCustomer();
+         return list;
+    }
+}
