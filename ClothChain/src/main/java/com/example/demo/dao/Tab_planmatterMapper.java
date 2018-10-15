71b5528cb7edb783a38cac520ff57c22ff5bcfea
cmyjiayou <1179638351@qq.com>
2018-10-15 09:21:14 +0800

    a

--- ClothChain/src/main/java/com/example/demo/dao/Tab_planmatterMapper.java
+++ ClothChain/src/main/java/com/example/demo/dao/Tab_planmatterMapper.java
@@ -7,14 +7,24 @@ import org.apache.ibatis.annotations.Param;
 
 public interface Tab_planmatterMapper {
     int countByExample(Tab_planmatterExample example);
+
     int deleteByExample(Tab_planmatterExample example);
+
     int deleteByPrimaryKey(Integer planmatterid);
+
     int insert(Tab_planmatter record);
+
     int insertSelective(Tab_planmatter record);
+
     List<Tab_planmatter> selectByExample(Tab_planmatterExample example);
+
     Tab_planmatter selectByPrimaryKey(Integer planmatterid);
+
     int updateByExampleSelective(@Param("record") Tab_planmatter record, @Param("example") Tab_planmatterExample example);
+
     int updateByExample(@Param("record") Tab_planmatter record, @Param("example") Tab_planmatterExample example);
+
     int updateByPrimaryKeySelective(Tab_planmatter record);
+
     int updateByPrimaryKey(Tab_planmatter record);
 }
